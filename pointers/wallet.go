245a5208ff8a5a019b5dd3d6861e53d00740a783
Matt <MF-Billings@users.noreply.github.com>
2021-03-14 12:15:08 -0400

    create new Bitcoin type based off type int

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,15 +1,17 @@
 package main
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	// ^ if the * is excluded the balance of a copy of w changes, not w itself
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	// ^ method receiver type is kept the same for consistency
 	return w.balance
 }
