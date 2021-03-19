889384eaa1b5835a1a09cf9077e834d1b5920e86
Matt <MF-Billings@users.noreply.github.com>
2021-03-18 21:36:06 -0400

    add dependency injection section

---
+++ di/di_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Matt")
+
+	got := buffer.String()
+	want := "Hello, Matt"
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
