f3171738746108720b939ad77f45b6ce695b3b7d
Matt <MF-Billings@users.noreply.github.com>
2021-03-18 21:42:34 -0400

    add a Greet function that uses HTTP

--- di/di.go
+++ di/di.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
 func main() {
-	Greet(os.Stdout, "Matt")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
 }
